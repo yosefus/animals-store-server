2cbf406181c45f0d9ba16a511262643761de8f93
yosefus <yosalsoni@gmail.com>
2024-10-20 19:30:31 +0300

    try to fix cors

--- src/main.ts
+++ src/main.ts
@@ -5,14 +5,14 @@ import * as cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser());
-
   app.enableCors({
     origin: ['http://localhost:5173', 'https://animals-store-client.vercel.app'],  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
-    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
   });
+
+  app.use(cookieParser());
+
 
   await app.listen(3000);
 }
